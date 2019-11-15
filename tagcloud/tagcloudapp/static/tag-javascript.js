61edbf7efe76c3fa0efcd0dce35a2b970840fddb
Daci54 <d.stojkovic93@gmail.com>
2019-11-15 02:13:10 +0100

    all 3 select boxes dynamic

--- tagcloud/tagcloudapp/static/tag-javascript.js
+++ tagcloud/tagcloudapp/static/tag-javascript.js
@@ -1,10 +1,16 @@
 document.addEventListener('DOMContentLoaded', function() {
     
         let projectselect = document.getElementById('projectselect');
+        let newop = new Option("Select", "select", true, true);
+        projectselect.add(newop, 0);
         let wpsselect = document.getElementById('workpackageselect');
         let subselect = document.getElementById('subjectselect');
 
         projectselect.onchange = () => {
+            wpsselect.options.length=0;
+            wpsselect.options[0]=new Option("Select", "select", true, true);
+            subselect.options.length=0;
+            subselect.options[0]=new Option("Select", "select", true, true);
             projectid = projectselect.value;
             axios.post('/projectselect', {
                     prid: projectid                
@@ -12,28 +18,33 @@ document.addEventListener('DOMContentLoaded', function() {
             .then (response => {
                 let wpselHTML = '';
                 for (let wp of response.data.wps) {
-                    wpselHTML += '<option value="' + wp.id + '">' + wp.name + '</option>';
+                    // wpselHTML += '<option value="' + wp.id + '">' + wp.name + '</option>';
+                    var op = document.createElement("option");
+                    op.textContent = wp.name;
+                    op.value = wp.id;
+                    wpsselect.append(op);
                 }
 
-                wpsselect.innerHTML = wpselHTML;
+                // wpsselect.innerHTML = wpselHTML;
 
             });
         }
 
 
-
         workpackageselect.onchange = () => {
+            subselect.options.length=0;
+            subselect.options[0]=new Option("Select", "select", true, true);
             wpid = wpsselect.value;
             axios.post('/wpselect', {
                     wpid: wpid                
             })
             .then (response => {
-                let subselHTML = '';
                 for (let sub of response.data.subs) {
-                    subselHTML += '<option value="' + sub.id + '">' + sub.name + '</option>';
+                    var op = document.createElement("option");
+                    op.textContent = sub.name;
+                    op.value = sub.id;
+                    subselect.append(op);
                 }
-
-                subselect.innerHTML = subselHTML;
 
             });
         }
